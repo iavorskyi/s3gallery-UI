23e5700975555b790d9064897423dc2da5636857
Iavorskyi Oleksandr <a7038329@gmail.com>
2022-09-09 00:44:51 +0300

    Delete cookie mysession when log out

--- s3_gallery_ui/src/Header/index.jsx
+++ s3_gallery_ui/src/Header/index.jsx
@@ -20,6 +20,7 @@ export const Header = (props) =>  {
         try {
             const url = '/sign-out'
             const response = await axios.get(url);
+            document.cookie = "mysession=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
         } catch (error) {
             if(axios.isCancel(error)){
                 console.log('Failed to log out');
